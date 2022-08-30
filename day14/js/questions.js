bae4b9c512944ee54bc42ca2b76b7d9d8fb9a8fc
Jhonata Alves <jhonatab4@gmail.com>
2022-08-30 02:47:54 -0300

    day14

---
+++ day14/js/questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Qual foi o quinto desafio dos 21diasdecodigo da Rocketseat?",
+    answer: "Desenvolver uma página 404.",
+    options: [
+      "Criar uma Calculadora.",
+      "Desenvolver uma página 404.",
+      "Desenvolver um contador.",
+      "Fazer um Hello Wolrd."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual foi o décimo desafio dos 21diasdecodigo da Rocketseat?",
+    answer: "Usar lib Particle.js.",
+    options: [
+      "Desenvolver um Toggle que alterna o tema light/dark",
+      "Desenvolver uma Galleria de Imagens",
+      "Criar um form de login",
+      "Usar lib Particle.js."
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual será o décimo sétimo desafio dos 21diasdecodigo da Rocketseat?",
+    answer: "Gerador de Senhas Aleatórias.",
+    options: [
+      "Site que Exiba vitrine de produtos de um e-comerce",
+      "Gerador de Senhas Aleatórias.",
+      "Site de Imagens Aleatórias.",
+      "Site para Selecionar Emojis."
+    ]
+  }
+];
